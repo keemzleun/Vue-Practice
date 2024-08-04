db778b934dfb5e0d7c6c947abde559deca7048ee
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-08-04 20:29:35 +0900

    drag&drop

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,115 @@
 <template>
-  <h1>영화 정보</h1>
-  <div v-for="(movie, i) in data" :key="i">
-    <!-- :속성명="데이터" -->
-    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
-    <p>개봉: {{ movie.year}}</p>
-    <p>장르: {{movie.category}}</p>
-    <button @:click="increaseLike(i)">좋아요</button> 
-    <span>{{ movie.like }}</span>
+  <div id="demo-container">
+    <DxScrollView id="scroll">
+      <DxDraggable
+        id="list"
+        :group="draggingGroupName"
+        :on-drag-start="onListDragStart"
+        data="dropArea"
+      >
+        <DxDraggable
+          v-for="task in tasks"
+          :key="task.text"
+          :clone="true"
+          :group="draggingGroupName"
+          :data="task"
+          :on-drag-start="onItemDragStart"
+          :on-drag-end="onItemDragEnd"
+          class="item dx-card"
+        >
+          {{ task.text }}
+        </DxDraggable>
+      </DxDraggable>
+    </DxScrollView>
+
+    <DxScheduler
+      time-zone="America/Los_Angeles"
+      id="scheduler"
+      :data-source="appointments"
+      :current-date="currentDate"
+      :views="views"
+      :height="600"
+      :start-day-hour="9"
+      :editing="true"
+    >
+      <DxAppointmentDragging
+        :group="draggingGroupName"
+        :on-remove="onAppointmentRemove"
+        :on-add="onAppointmentAdd"
+      />
+    </DxScheduler>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import DxScheduler, { DxAppointmentDragging, DxSchedulerTypes } from 'devextreme-vue/scheduler';
+import DxDraggable, { DxDraggableTypes } from 'devextreme-vue/draggable';
+import DxScrollView from 'devextreme-vue/scroll-view';
+import { appointments as appointmentsData, tasks as tasksData, Task } from '@/data';
 
-<script>
-export default {
-  name: 'App',
-  data() {
-    // 상태 변수(state value) : 화면에 자주 업데이트 되는 변수들을 정의함
-    return {
-      data: [
-        {
-          title: "노량",
-          year: 2023,
-          category: "액션, 드라마",
-          textRed: "color: red",
-          like: 0,
-        },
-        {
-          title: "아쿠아맨",
-          year: 2023,
-          category: "액션, 판타지",
-          like: 0,
-        },
-      ],  
-    }
-  },
-  methods: {
-    increaseLike(i) {
-      this.data[i].like += 1;
-    }
+const draggingGroupName = ref('appointmentsGroup');
+const views = ref([{ type: 'day', intervalCount: 4 }]);
+const currentDate = ref(new Date(2021, 3, 26));
+const tasks = ref<Task[]>(tasksData);
+const appointments = ref<DxSchedulerTypes.Appointment[]>(appointmentsData);
+
+function onAppointmentRemove({ itemData }: DxSchedulerTypes.AppointmentDraggingRemoveEvent) {
+  const index = appointments.value.indexOf(itemData);
+
+  if (index >= 0) {
+    appointments.value = [...appointments.value];
+    appointments.value.splice(index, 1);
+    tasks.value = [...tasks.value, itemData];
+  }
+}
+function onAppointmentAdd(e: DxSchedulerTypes.AppointmentDraggingAddEvent) {
+  const index = tasks.value.indexOf(e.fromData);
+
+  if (index >= 0) {
+    tasks.value = [...tasks.value];
+    tasks.value.splice(index, 1);
+    appointments.value = [...appointments.value, e.itemData];
+  }
+}
+function onListDragStart(e: DxDraggableTypes.DragStartEvent) {
+  e.cancel = true;
+}
+function onItemDragStart(e: DxDraggableTypes.DragStartEvent) {
+  e.itemData = e.fromData;
+}
+function onItemDragEnd(e: DxDraggableTypes.DragEndEvent) {
+  if (e.toData) {
+    e.cancel = true;
   }
 }
 </script>
-
 <style>
-.bg-yellow {
-  background: gold;
-  padding: 10px;
+#scroll,
+#list {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 240px;
+}
+
+.item {
+  color: var(--dx-color-text);
+  background-color: var(--dx-component-color-bg);
+  box-sizing: border-box;
+  padding: 10px 20px;
+  margin-bottom: 10px;
+}
+
+#scheduler {
+  margin-left: 270px;
+}
+
+.dx-draggable-source {
+  opacity: 0.5;
+}
+
+.dx-draggable-dragging > * {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 6px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
