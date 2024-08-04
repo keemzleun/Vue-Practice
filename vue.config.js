6abd61f7fc6701eedd6b330da14d92ffeb533654
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-08-05 08:54:56 +0900

    ts version

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .loader('ts-loader')
+      .tap(options => {
+        options.transpileOnly = true;
+        return options;
+      });
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions = {
+          isCustomElement: tag => tag.startsWith('dx-')
+        };
+        return options;
+      });
+  }
 })
